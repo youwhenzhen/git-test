252867cf3dd5ac9c1768dfc3e71f72a57f75777b
youwhenzhen <76403678+youwhenzhen@users.noreply.github.com>
2021-10-13 20:15:30 +0800

    hhh

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,7 +10,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hhhhhhhhhhhhhhhhhhhhhhhh");
 
-        System.out.println("2222222222222222222222222222");
+        System.out.println("2");
 
         System.out.println("333333333333333333333333");
 
